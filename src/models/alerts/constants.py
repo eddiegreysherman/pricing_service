3ce9d588ffe69deaa3d32f73f40211dde32cf37f
eddie.sherman@gmail.com <Gr3750n123>
2017-08-15 21:53:02 -0400

    completed heroku configs

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,6 +1,8 @@
-URL = "https://api.mailgun.net/v3/sandboxdded854c436743fd90815650d978dbe7.mailgun.org/messages"
-API_KEY = "key-a8274dad54a2f5443d1617ead3789278"
-FROM = "Mailgun Sandbox <postmaster@sandboxdded854c436743fd90815650d978dbe7.mailgun.org>"
+import os
+
+URL = os.environ.get("MAILGUN_URL")
+API_KEY = os.environ.get("MAILGUN_API_KEY")
+FROM = os.environ.get("MAILGUN_FROM")
 
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
