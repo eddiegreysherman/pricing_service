46353be7d19c8797d120b15883416df2addacccf
eddie.sherman@gmail.com <Gr3750n123>
2017-08-16 13:49:11 -0400

    Fixed it finally...

--- src/run.py
+++ src/run.py
@@ -1,4 +1,4 @@
 from src.app import app
 import os
 
-app.run(host="0.0.0.0", debug=app.config['DEBUG'], port=os.environ.get(port))
+app.run(host="0.0.0.0", debug=app.config['DEBUG'], port=int(os.environ.get(port)))
